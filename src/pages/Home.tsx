843faba0222e897de620580086d1efc9d626ba20
krishraj777 <rajudantuluri777@gmail.com>
2020-04-01 19:57:00 +1100

    First Ionic Assignment

---
+++ src/pages/Home.tsx
@@ -0,0 +1,27 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React from 'react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Home.css';
+
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Surya Ionic Project</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding">
+        the world is ...
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Blank</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <ExploreContainer />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
