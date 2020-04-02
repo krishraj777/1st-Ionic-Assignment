b54bb9638ee20dbc636459601b9a928384d84827
krishraj777 <rajudantuluri777@gmail.com>
2020-04-02 15:08:30 +1100

    1st small change in text of <p>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,10 @@
 import { add } from ‘ionicons /icons’;
-import { add, settings, share, person, arrowForwardCircle, arrowBackCircle, arrowUpCircle, logoVimeo, logoFacebook, logoInstagram, logoTwitter } from 'ionicons/icons';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 import { attachProps } from '@ionic/react/dist/types/components/utils';
-
+import { add, settings, share, person, arrowForwardCircle, arrowBackCircle, arrowUpCircle, logoVimeo, logoFacebook, logoInstagram, logoTwitter } from 'ionicons/icons';
 const Home: React.FC = () => {
   return (
     <IonPage>
@@ -16,6 +15,16 @@ const Home: React.FC = () => {
       </IonHeader>
       <IonContent className="ion-padding">
         the world is oyster
+        <p>
+          if you get lost, the {' '}
+         <a
+         target="_blank"
+         rel="nooper"
+         href="https://ionicframework.com/docs">
+           docs </a> {' '}
+           will be your guide
+           </p>
+      
         <IonHeader collapse="condense">
         <IonFab vertical="bottom" horizontal="end" slot="fixed">
     <IonFabButton>
